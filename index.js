b7e4d2cf154002bb3317590d06a894f6cc90913d
Justin Wu <justinhwu95@gmail.com>
2019-03-04 20:49:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -73,7 +73,9 @@ function placeOrder(cardNumber) {
   
   if(cardNumber){
   
-  return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  cart = []
+  return cart
   
   }
   
