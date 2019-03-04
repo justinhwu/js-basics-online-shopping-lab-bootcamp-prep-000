6aed27ecbf97489ac74c671170dc54b56c8b0cc1
Justin Wu <justinhwu95@gmail.com>
2019-03-04 20:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   
-  while(cardNumber){
+  if(cardNumber){
   
   return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   
